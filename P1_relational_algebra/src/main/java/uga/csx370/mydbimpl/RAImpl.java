9aa1fdd8bda346ce58e44d36ba0414a908290eee
Pushya Damania <phd87389@uga.edu>
2025-09-23 21:23:51 -0400

    Update RAImpl.java
    intersect and diff done

--- P1_relational_algebra/src/main/java/uga/csx370/mydbimpl/RAImpl.java
+++ P1_relational_algebra/src/main/java/uga/csx370/mydbimpl/RAImpl.java
@@ -28,14 +28,47 @@ public class RAImpl implements RA {
 
     @Override
     public Relation intersect(Relation rel1, Relation rel2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'intersect'");
+        // A and B = A - (A - B)
+        return diff(rel1, diff(rel1, rel2));
     }
 
     @Override
     public Relation diff(Relation rel1, Relation rel2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'diff'");
+        //make new relation with same schema as rel1
+        Relation retrel = new RelationBuilder()
+            .attributeNames(rel1.getAttrs())
+            .attributeTypes(rel1.getTypes())
+            .build();
+
+        //for row in rel1, check if exists in rel2
+        for (int i = 0; i < rel1.getSize(); i++) {
+            int found = 0;
+            for (int j = 0; j < rel2.getSize(); j++) {
+                if ((rel1.getRow(i)).equals(rel2.getRow(j))) {
+                    found = 1;
+                    break;
+                }
+            }
+            //if not in rel2 add to retrel
+            if (found == 0) {
+                if (retrel.getSize() == 0) { //if retrel empty, add
+                    retrel.insert(rel1.getRow(i));
+                } else { //else check 4 dupes
+                    int match = 0;
+                    for (int k = 0; k < retrel.getSize(); k++) {
+                        if ((rel1.getRow(i)).equals(retrel.getRow(k))) {
+                            match = 1;
+                            break;
+                        }
+                    }
+                    //if not dupe, add to retrel
+                    if (match == 0) {
+                        retrel.insert(rel1.getRow(i));
+                    }
+                }
+            }
+        }
+        return retrel;
     }
 
     @Override
