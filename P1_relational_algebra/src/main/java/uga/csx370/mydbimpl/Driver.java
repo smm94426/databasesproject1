0fe2bae32016462269649adcd9fb59d87dbe1c38
Pushya Damania <phd87389@uga.edu>
2025-09-24 19:54:27 -0400

    Update Driver.java

--- P1_relational_algebra/src/main/java/uga/csx370/mydbimpl/Driver.java
+++ P1_relational_algebra/src/main/java/uga/csx370/mydbimpl/Driver.java
@@ -58,6 +58,7 @@ public class Driver {
         shafatQuery(takesRel, courseRel, instructorRel, teachesRel, ra);
         meghanaQuery(instructorRel, teachesRel, ra);
         seanQuery(studentRel, advisorRel, teachesRel, takesRel, instructorRel, ra);
+        PushyaQuery(studentRel, takesRel, courseRel, instructorRel, teachesRel, ra);
         
     } // main
 
@@ -264,4 +265,154 @@ public class Driver {
         Relation idName = ra.project(joined, List.of("ID", "name"));
         idName.print();
     } //meghanaQuery
+
+    public static void PushyaQuery(Relation studentRel, Relation takesRel, Relation courseRel,
+                             Relation instructorRel, Relation teachesRel, RA ra) {
+
+        System.out.println("\n=== Pushya's Query ===");
+        System.out.println("\n=== note, slow ===");    
+
+        //Written before predicateImpl.java so it has to override manually, no time to change
+        
+        //Select students who took courses after 2005
+        Relation takesAfter2005 = ra.select(takesRel, new Predicate() {
+            @Override
+            public boolean check(List<Cell> row) {
+                return row.get(takesRel.getAttrIndex("year")).getAsInt() > 2005;
+            }
+            @Override
+            public boolean evaluate(List<Cell> row, List<String> attrs) {
+                return check(row);
+            }
+        });
+
+        //Keep student ID + course ID
+        Relation t1 = ra.project(takesAfter2005, List.of("ID", "course_id"));
+
+        //Rename teaches/instructor + join
+        Relation teachesRenamed = ra.rename(teachesRel, List.of("ID"), List.of("t_ID"));
+        Relation instrRenamed = ra.rename(instructorRel, List.of("ID"), List.of("i_ID"));
+
+        Relation teachesInstr = ra.join(teachesRenamed, instrRenamed, new Predicate() {
+            @Override
+            public boolean check(List<Cell> row) {
+                int offset = teachesRenamed.getAttrs().size();
+                return row.get(teachesRenamed.getAttrIndex("t_ID"))
+                        .equals(row.get(offset + instrRenamed.getAttrIndex("i_ID")));
+            }
+            @Override
+            public boolean evaluate(List<Cell> row, List<String> attrs) {
+                return check(row);
+            }
+        });
+
+        Relation t2 = ra.project(teachesInstr, List.of("course_id", "t_ID"));
+        Relation t2Renamed = ra.rename(t2, List.of("course_id"), List.of("course_id_t2"));
+
+        //Join w students
+        Relation t3 = ra.join(t1, t2Renamed, new Predicate() {
+            @Override
+            public boolean check(List<Cell> row) {
+                int offset = t1.getAttrs().size();
+                return row.get(t1.getAttrIndex("course_id"))
+                        .equals(row.get(offset + t2Renamed.getAttrIndex("course_id_t2")));
+            }
+            @Override
+            public boolean evaluate(List<Cell> row, List<String> attrs) {
+                return check(row);
+            }
+        });
+        Relation t3Proj = ra.project(t3, List.of("ID", "course_id", "t_ID"));
+
+        //Join w course info
+        Relation courseRenamed = ra.rename(courseRel, List.of("dept_name", "course_id"),
+                                        List.of("course_dept", "course_id_course"));
+        Relation t4 = ra.join(t3Proj, courseRenamed, new Predicate() {
+            @Override
+            public boolean check(List<Cell> row) {
+                int offset = t3Proj.getAttrs().size();
+                return row.get(t3Proj.getAttrIndex("course_id"))
+                        .equals(row.get(offset + courseRenamed.getAttrIndex("course_id_course")));
+            }
+            @Override
+            public boolean evaluate(List<Cell> row, List<String> attrs) {
+                return check(row);
+            }
+        });
+        Relation t4Proj = ra.project(t4, List.of("ID", "t_ID", "course_dept"));
+
+        //Self join for multi-department instr
+        Relation t4_a = ra.rename(t4Proj, List.of("ID", "t_ID", "course_dept"),
+                                List.of("ID_a", "t_ID_a", "course_dept_a"));
+        Relation t4_b = ra.rename(t4Proj, List.of("ID", "t_ID", "course_dept"),
+                                List.of("ID_b", "t_ID_b", "course_dept_b"));
+
+        Relation multiDeptPairs = ra.join(t4_a, t4_b, new Predicate() {
+            @Override
+            public boolean check(List<Cell> row) {
+                int offset = t4_a.getAttrs().size();
+                return row.get(t4_a.getAttrIndex("ID_a")).equals(row.get(offset + t4_b.getAttrIndex("ID_b")))
+                        && row.get(t4_a.getAttrIndex("t_ID_a")).equals(row.get(offset + t4_b.getAttrIndex("t_ID_b")))
+                        && !row.get(t4_a.getAttrIndex("course_dept_a")).equals(row.get(offset + t4_b.getAttrIndex("course_dept_b")));
+            }
+            @Override
+            public boolean evaluate(List<Cell> row, List<String> attrs) {
+                return check(row);
+            }
+        });
+
+        Relation qualifiedPairs = ra.project(multiDeptPairs, List.of("ID_a", "t_ID_a"));
+        Relation distinctQualified = ra.union(qualifiedPairs, qualifiedPairs);
+        Relation sIDs = ra.rename(distinctQualified, List.of("ID_a", "t_ID_a"), List.of("ID", "t_ID"));
+
+        //Join w stu 4 names
+        Relation studentRenamed = ra.rename(studentRel, List.of("ID"), List.of("ID_stu"));
+        Relation result = ra.join(sIDs, studentRenamed, new Predicate() {
+            @Override
+            public boolean check(List<Cell> row) {
+                int offset = sIDs.getAttrs().size();
+                return row.get(sIDs.getAttrIndex("ID"))
+                        .equals(row.get(offset + studentRenamed.getAttrIndex("ID_stu")));
+            }
+            @Override
+            public boolean evaluate(List<Cell> row, List<String> attrs) {
+                return check(row);
+            }
+        });
+
+        Relation finalResult = ra.project(result, List.of("ID", "name"));
+
+        //Filter high-credit students
+        int threshold = 128;
+        Relation highCredStudents = ra.select(studentRel, new Predicate() {
+            @Override
+            public boolean check(List<Cell> row) {
+                return row.get(studentRel.getAttrIndex("tot_cred")).getAsInt() >= threshold;
+            }
+            @Override
+            public boolean evaluate(List<Cell> row, List<String> attrs) {
+                return check(row);
+            }
+        });
+
+        Relation highCredSIDsRenamed = ra.rename(ra.project(highCredStudents, List.of("ID")),
+                                                List.of("ID"), List.of("ID_hc"));
+
+        Relation filteredFinal = ra.join(finalResult, highCredSIDsRenamed, new Predicate() {
+            @Override
+            public boolean check(List<Cell> row) {
+                int offset = finalResult.getAttrs().size();
+                return row.get(finalResult.getAttrIndex("ID"))
+                        .equals(row.get(offset + highCredSIDsRenamed.getAttrIndex("ID_hc")));
+            }
+            @Override
+            public boolean evaluate(List<Cell> row, List<String> attrs) {
+                return check(row);
+            }
+        });
+
+        filteredFinal = ra.project(filteredFinal, List.of("ID", "name"));
+        System.out.println("Pushya Query " + filteredFinal.getSize());
+        filteredFinal.print();
+    }// Pushya query
 }
